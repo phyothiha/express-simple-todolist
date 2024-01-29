4810ff744495f0b1c27781ab0642b804cc13d4ae
Phyo Thiha <phyothiha96@gmail.com>
2024-01-30 00:21:59 +0630

    simple crud

---
+++ README.md
@@ -0,0 +1,35 @@
+# Instructions
+
+### Follow below steps, by using terminal, to run on local machine.
+
+1. First open **Git Bash App** and change directory to **Desktop**
+
+```
+cd Desktop
+```
+
+1. Clone this public repo.
+
+```
+git clone https://github.com/phyothiha/express-simple-todolist.git
+```
+
+2. Change directory.
+
+```
+cd express-simple-todolist
+```
+
+3. Install packages
+
+```
+npm install
+```
+
+4. Run app
+
+```
+npm run dev
+```
+
+> This app is running on http://127.0.0.1:7000
---
+++ index.js
@@ -0,0 +1,195 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const port = 7000;
+
+//  parses incoming requests with JSON payloads
+app.use(express.json());
+
+// connect to database
+mongoose
+  .connect(
+    "mongodb+srv://root:1qaz!QAZ@cluster0.d12o3ry.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Database connected successfully!"))
+  .catch((err) => console.error(err));
+
+// create task schema
+const taskSchema = new mongoose.Schema(
+  {
+    uuid: {
+      type: String,
+      unique: true,
+    },
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+// complie schema into model
+const Task = mongoose.model("Task", taskSchema);
+
+// index
+app.get("/api/tasks", async (req, res) => {
+  try {
+    // sort latest task with isCompleted false
+    const tasks = await Task.find().sort({ createdAt: -1 }).exec();
+
+    res.json({
+      data: tasks,
+      meta: {
+        total: tasks.length,
+      },
+    });
+  } catch (err) {
+    res.json({
+      message: err.message,
+    });
+  }
+});
+
+// create
+app.post("/api/tasks", async (req, res) => {
+  const name = req.body.name?.trim();
+
+  // req name value validation
+  if (name == undefined) {
+    return res.json({
+      message: "Task name is required!",
+    });
+  }
+
+  if (name.length < 5) {
+    return res.json({
+      message: "Task name should be more than 5 characters!",
+    });
+  }
+
+  const task = new Task({
+    uuid: uuidv4(),
+    name,
+  });
+
+  try {
+    const newTask = await task.save();
+
+    res.json({
+      message: "Task created successfully!",
+      data: newTask,
+    });
+  } catch (err) {
+    res.json({
+      message: err.message,
+    });
+  }
+});
+
+// update
+app.put(
+  "/api/tasks/:taskId",
+  async (req, res, next) => {
+    const task = await Task.findOne({ uuid: req.params.taskId });
+
+    if (!task) {
+      return res.json({
+        message: `TaskID ${req.params.taskId} not found!`,
+      });
+    }
+
+    // The variables set on res.locals are available within a single request-response cycle, and will not be shared between requests.
+    res.locals.task = task;
+
+    next();
+  },
+  async (req, res, next) => {
+    const localTask = res.locals.task;
+
+    const name = req.body.name?.trim();
+    const { isCompleted } = req.body;
+
+    if (name == undefined || isCompleted == undefined) {
+      return res.json({
+        message: "Task name or isCompleted status is required!",
+      });
+    }
+
+    if (name.length < 5) {
+      return res.json({
+        message: "Task name should be more than 5 characters!",
+      });
+    }
+
+    // name should be unique
+    const isTaskNameExists = await Task.findOne({ name });
+
+    if (isTaskNameExists && req.params.taskId != isTaskNameExists.uuid) {
+      return res.json({
+        message: "Task name should be unique",
+      });
+    }
+
+    try {
+      const updatedTask = await Task.findOneAndUpdate(
+        { uuid: req.params.taskId },
+        { name, isCompleted },
+        {
+          new: true,
+        }
+      );
+
+      res.json({
+        message: `TaskID ${req.params.taskId} updated successfully!`,
+        data: updatedTask,
+      });
+    } catch (err) {
+      res.json({
+        message: err.message,
+      });
+    }
+  }
+);
+
+// delete
+app.delete(
+  "/api/tasks/:taskId",
+  async (req, res, next) => {
+    const task = await Task.findOne({ uuid: req.params.taskId });
+
+    if (!task) {
+      return res.json({
+        message: `TaskID ${req.params.taskId} not found!`,
+      });
+    }
+
+    next();
+  },
+  async (req, res, next) => {
+    try {
+      await Task.deleteOne({ uuid: req.params.taskId });
+
+      res.json({
+        message: `TaskID ${req.params.taskId} deleted successfully!`,
+      });
+    } catch (err) {
+      res.json({
+        message: err.message,
+      });
+    }
+  }
+);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
